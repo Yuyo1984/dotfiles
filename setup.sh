e54a4a02201cdc212ae9af087c9559f5db4e874d
Yuyo Yanase <yuyoyanase@MBP-Main.local>
2023-11-24 21:11:41 +0900

    管理するファイル追加

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
  #!/bin/zsh
 
-  DOT_FILES=(.zshrc .vimrc .tmux.conf)
+  DOT_FILES=(.gem .gemrc .git .gitconfig \
+ .procon-gardener .pyenv .rbenv .zprofile .zsh_sessions \
+ .zshrc .vimrc .tmux.conf)
 
  for file in ${DOT_FILES[@]}
  do
-     ln -s $HOME/dotfiles/$file $HOME/$file
+     ln -s $HOME/tools/dotfiles/$file $HOME/$file
  done
